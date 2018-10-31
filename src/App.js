fe3d37d60758c51e2051835a0b9e42cda607c704
JohnAlvarezWipro <JohnAlvarezWipro@users.noreply.github.com>
2018-10-31 12:11:20 -0500

    Have front of file dropper working on carrier details page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope, faEllipsisV, faFileAlt, faSlidersH, faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faEllipsisV, faFileAlt, faSlidersH, faClipboard, faUpload } from '@fortawesome/free-solid-svg-icons';
 import "./App.css";
 import Console from "./containers/Console/Console";
 library.add(faEnvelope);
@@ -9,6 +9,7 @@ library.add(faEllipsisV);
 library.add(faFileAlt);
 library.add(faSlidersH);
 library.add(faClipboard);
+library.add(faUpload);
 class App extends Component {
   
   render() {
--- src/components/FileDropper/FileDropper.js
+++ src/components/FileDropper/FileDropper.js
@@ -1,11 +1,62 @@
-import React from 'react';
-import './FileDropper.css';
+import React, { Component } from "react";
+import "./FileDropper.css";
+import Dropzone from "react-dropzone";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const FileDropper = props => {
-  return (
-    <div className="FileDropper">
-      <h1>hi</h1>
-    </div>
-  );
-};
+class FileDropper extends Component {
+  constructor() {
+    super();
+    this.state = {
+      files: []
+    };
+  }
+
+  onDrop(files) {
+    this.setState({
+      files: files.map(file => ({
+        ...file,
+        preview: URL.createObjectURL(file)
+      }))
+    });
+  }
+
+  componentWillUnmount() {
+    // Make sure to revoke the data uris to avoid memory leaks
+    const { files } = this.state;
+    for (let i = files.length; i >= 0; i--) {
+      const file = files[0];
+      URL.revokeObjectURL(file.preview);
+    }
+  }
+
+  render() {
+    const { files } = this.state;
+
+    const thumbs = files.map(file => (
+      <div className="thumb">
+        
+        <div className="thumbInner">
+        
+          <h5>Your Logo: </h5>
+          <hr/>
+          <img className="thumb-img" src={file.preview} alt="thumb"/>
+        </div>
+      </div>
+    ));
+
+    return (
+      <div className="FileDropper">
+        <section>
+          <div className="dropzone">
+            <Dropzone accept="image/*" onDrop={this.onDrop.bind(this)}>
+              <FontAwesomeIcon icon="upload" />
+              <p>Try dropping some files here, or click so select files to upload.</p>
+            </Dropzone>
+          </div>
+          <aside className="thumbsContainer">{thumbs}</aside>
+        </section>
+      </div>
+    );
+  }
+}
 export default FileDropper;
